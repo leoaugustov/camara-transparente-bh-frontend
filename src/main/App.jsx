c955d2c6bf05668c402a9f038d0cf1942d0ff00a
leoaugustov <vieira.augustoleonardo@gmail.com>
2020-04-24 10:29:22 -0300

    Busca de dados extraída para fora do componente TabelaVereadores

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Card from 'react-bootstrap/Card'
 import TabelaVereadores from './componentes/TabelaVereadores'
 
 import './App.css'
 
 export default function App() {
+    const [vereadores, setVereadores] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/vereadores')
+            .then(resposta => resposta.json())
+            .then(vereadores => setVereadores(vereadores))
+    }, [])
+
     return (
         <>
-        <div className="barra-superior texto-lg fixed-top border-bottom shadow-sm d-flex justify-content-center align-items-center">
+        <div className="barra-superior fixed-top border-bottom shadow-sm d-flex justify-content-center align-items-center">
             <span className="font-weight-bold">câmara</span>
             <span className="font-weight-bold text-info">transparenteBH</span>
         </div>
@@ -18,12 +25,7 @@ export default function App() {
             <Row className="text-secondary">
                 <Col>Todas informações são referentes apenas ao último mandato.</Col>
             </Row>
-            <Card className="shadow mb-3">
-                <Card.Header className="font-weight-bold texto-md">Vereadores</Card.Header>
-                <Card.Body className="p-0">
-                    <TabelaVereadores/>
-                </Card.Body>
-            </Card>
+            <TabelaVereadores vereadores={ vereadores }/>
         </Container>
         </>  
     );
--- src/main/componentes/TabelaVereadores.jsx
+++ src/main/componentes/TabelaVereadores.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import BootstrapTable from 'react-bootstrap-table-next'
+import Card from 'react-bootstrap/Card'
 
 import './TabelaVereadores.css'
 import fotoPadraoVereador from '../../imagens/vereador-foto-padrao.png'
 
-export default function TabelaVereadores() {
+export default function TabelaVereadores({ vereadores }) {
     const formatoMonetario = new Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'})
     const formatoPorcentagem = new Intl.NumberFormat('pt-BR', {style: 'percent', maximumFractionDigits: 2})
 
@@ -56,21 +57,20 @@ export default function TabelaVereadores() {
         coluna.classes = 'align-middle'
     })
 
-    const [vereadores, setVereadores] = useState([])
-
-    useEffect(() => {
-        fetch('http://localhost:8080/api/vereadores')
-            .then(resposta => resposta.json())
-            .then(vereadores => setVereadores(vereadores))
-    }, [])
-
-    return <BootstrapTable 
-        keyField="id" 
-        data={ vereadores } 
-        columns={ colunas } 
-        bootstrap4 
-        bordered={ false } 
-        noDataIndication={ () => "Nenhum vereador encontrado..." } 
-        wrapperClasses="table-responsive" 
-        classes="mb-0"/>
+    return (
+        <Card className="shadow mb-3">
+            <Card.Header className="titulo-cartao">Vereadores</Card.Header>
+            <Card.Body className="p-0">
+                <BootstrapTable 
+                    keyField="id" 
+                    data={ vereadores } 
+                    columns={ colunas } 
+                    bootstrap4 
+                    bordered={ false } 
+                    noDataIndication={ () => "Nenhum vereador encontrado..." } 
+                    wrapperClasses="table-responsive" 
+                    classes="mb-0"/>
+            </Card.Body>
+        </Card>
+    )    
 }
