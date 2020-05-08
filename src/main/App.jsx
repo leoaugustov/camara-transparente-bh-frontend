f13a62ad1faca3a840b038509fbab192990a549b
leoaugustov <vieira.augustoleonardo@gmail.com>
2020-05-07 22:00:37 -0300

    Mensagem data última atualização implementada

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -11,7 +11,16 @@ import './App.css'
 import icone from '../imagens/icone.png'
 
 export default function App() {
+    const formatoData = new Intl.DateTimeFormat('pt-BR', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+    })
+
     const [vereadores, setVereadores] = useState([])
+    const [dataUltimaAtualizacao, setDataUltimaAtualizacao] = useState([])
 
     useEffect(() => {
         fetch('http://localhost:8080/api/vereadores')
@@ -27,6 +36,12 @@ export default function App() {
                 return vereadores
             })
             .then(setVereadores)
+
+        fetch('http://localhost:8080/api/status-scrap')
+            .then(resposta => resposta.json())
+            .then(status => {
+                setDataUltimaAtualizacao(new Date(status.dataUltimaAtualizacao))
+            })
     }, [])
 
     return (
@@ -38,7 +53,8 @@ export default function App() {
     </div>
     <Container fluid className="conteudo">
         <Row className="text-secondary my-3">
-            <Col>Todas informações são referentes apenas ao último mandato.</Col>
+            <Col xs={12} lg={8}>Todas informações são referentes apenas ao último mandato 😉.</Col>
+            <Col xs={12} lg={4}>☑️ Atualizado em { formatoData.format(dataUltimaAtualizacao) }</Col>
         </Row>
         <Sinteses vereadores={ vereadores }/>
         <Secao titulo="Custeio Parlamentar por Partido">
