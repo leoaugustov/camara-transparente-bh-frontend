e9f1f7ca1271361f9374aab968edca2bc38c7003
leoaugustov <vieira.augustoleonardo@gmail.com>
2020-05-16 00:13:02 -0300

    Tabela com listagem dos partidos adicionada

--- src/main/componentes/CusteioPorPartido.jsx
+++ src/main/componentes/CusteioPorPartido.jsx
@@ -1,17 +1,44 @@
 import React, { useState, useRef, useEffect } from 'react'
 import Chart from 'chart.js'
+import Tabela from './Tabela'
 import Card from 'react-bootstrap/Card'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+import './CusteioPorPartido.css'
 
 export default function CusteioPorPartido() {
+    const [formatoMonetario] = useState(new Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}))
     const canvasGrafico = useRef()
     const [, setGrafico] = useState([])
+    const [partidos, setPartidos] = useState([])
+
+    const colunasTabela = [{
+        dataField: 'siglaPartido',
+        text: 'Partido',
+        formatter: (celula, linha) => {
+            return (
+                <div className="text-nowrap d-flex">
+                    <div className="bloco-cor-legenda mr-2" style={ { backgroundColor: linha.cor } }/>
+                    <span>{ linha.siglaPartido }</span>
+                </div>
+            )
+        }
+    }, {
+        dataField: 'quantidadeVereadores',
+        text: 'Quant. Vereadores'
+    }, {
+        dataField: 'custeio',
+        text: 'Custeio Total',
+        formatter: (celula, linha, indiceLinha, extra) => extra.formato.format(celula),
+        formatExtraData: {formato: formatoMonetario}
+    }]
 
     useEffect(() => {
-        const formatoMonetario = new Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'})
-        
-        const options = {
+        const opcoesGrafico = {
             responsive: true, 
             maintainAspectRatio: false,
+            legend: { display: false },
             tooltips: {
                 callbacks: {
                     label: function(tooltipItem, data) {
@@ -26,17 +53,26 @@ export default function CusteioPorPartido() {
 
         fetch('http://localhost:8080/api/custeio-parlamentar/por-partido')
             .then(resposta => resposta.json())
-            .then(estruturarDados)
+            .then(dados => dados.filter(dado => dado.custeio > 0))
+            .then(ordenarCusteioDesc)
+            .then(adicionarCores)
             .then(dados => {
                 setGrafico(new Chart(canvasGrafico.current, {
                     type: 'doughnut',
-                    data: dados,
-                    options
+                    data: estruturarDadosGrafico(dados),
+                    options: opcoesGrafico
                 }))
-            })
-    }, [])
 
-    function estruturarDados(custeioPorPartido) {
+                return dados
+            })
+            .then(setPartidos)
+    }, [formatoMonetario])
+
+    function ordenarCusteioDesc(dados) {
+        return dados.sort((a, b) => b.custeio - a.custeio)
+    }
+
+    function adicionarCores(dados) {
         const cores = [
             'red',
             'indigo',
@@ -58,31 +94,41 @@ export default function CusteioPorPartido() {
             'coral'
         ]
 
+        return dados.map((dado, indice) => ( {...dado, cor: cores[indice]} ))
+    }
+
+    function estruturarDadosGrafico(dados) {
         const labels = []
         const data = []
+        const cores = []
 
-        for(let partido of custeioPorPartido) {
-            if(partido.custeio > 0) {
-                labels.push(partido.siglaPartido)
-                data.push(partido.custeio)
-            }    
+        for(let dado of dados) {
+            labels.push(dado.siglaPartido)
+            data.push(dado.custeio)
+            cores.push(dado.cor)
         }
 
-        return {
-            labels, 
-            datasets: [{ 
-                data, 
-                backgroundColor: cores
-            }]
-        }
+        return { labels, datasets: [{ data, backgroundColor: cores }] }
     }
 
     return (
     <Card className="shadow mb-3">
         <Card.Body>
-            <div style={ { height: '350px' } }>
-                <canvas ref={ canvasGrafico }></canvas>
-            </div> 
+            <Row>
+                <Col xs={12} lg={6} className="mb-3 mb-lg-0">
+                    <div style={ { height: '300px' } }>
+                        <canvas ref={ canvasGrafico }></canvas>
+                    </div>
+                </Col>
+                <Col xs={12} lg={6}>
+                    <Tabela 
+                        colunaChave="siglaPartido" 
+                        colunas={ colunasTabela } 
+                        dados={ partidos } 
+                        mensagemColecaoVazia="Nenhum partido encontrado..." 
+                        classesCssElementoWrapper="altura-tabela-partidos"/>
+                </Col>
+            </Row>
         </Card.Body>
     </Card>
     )
